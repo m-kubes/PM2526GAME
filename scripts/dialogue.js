21f2e91f4f19fb9187f6a601bd397a1e8dd8c783
KUBIAK <00michael.kubiak@students.kresa.org>
2026-04-20 13:42:38 -0400

    Added player dialogue options

--- scripts/dialogue.js
+++ scripts/dialogue.js
@@ -8,6 +8,7 @@ export default class Dialogue {
         this._dialogue_box.id = name + "_dialogue_box";
         this._dialogue_box.querySelector(".portrait img").src = image;
         this._dialogue_box.querySelector(".name").textContent = name;
+        this._options_menu = this._dialogue_box.querySelector(".options");
         document.body.appendChild(this._dialogue_box);
     }
 
@@ -21,6 +22,7 @@ export default class Dialogue {
             // the first timeout is here to queue it for next frame so the animation works
             setTimeout(() => {
                 this._dialogue_box.style.bottom = "1rem";
+                this._options_menu.style.bottom = "-100%";
 
                 // second timeout resolves promise after animation finishes
                 setTimeout(() => resolve('finished'), 1000);
@@ -29,27 +31,70 @@ export default class Dialogue {
     } 
 
     // not a promise cuz theres really no need for it
-    hide() {
+    hide(delay = 1000) {
         // animates box sliding back down to bottom
-        this._dialogue_box.style.bottom = "-100%";
         setTimeout(() => {
-            this._dialogue_box.style.display = "none";
-        }, 1000);
+            this._dialogue_box.style.bottom = "-100%";
+            this._options_menu.style.bottom = "0";
+
+            // timeout to hide the box after the animation finishes
+            setTimeout(() => {
+                this._dialogue_box.style.display = "none";
+            }, 1000);
+        }, delay);
     }
 
     // returns a promise that resolves when the text reveal is finished
-    reveal_text(new_text, text_speed = 30) {
+    reveal_text(new_text, text_speed = 30, response_options = []) {
         return new Promise((resolve) => {
             // uses a setInterval to reveal the text one character at a time
             const text_box = this._dialogue_box.querySelector(".text");
+            
             text_box.textContent = "";
             let index = 0
-            
+
             const typing_interval = setInterval(() => {
                 // stop interval when all text is done
+                // also present dialogue options
                 if (index >= new_text.length - 1) {
+                    const controller = new AbortController();
                     clearInterval(typing_interval);
-                    resolve('finished');
+                    
+                    // only provide options if there are any, otherwise just resolve the promise
+                    if (!response_options.length) {
+                        resolve();
+                    } else {
+                        // create a new button for each option and add it to the options menu
+                        const response_divs = []
+
+                        response_options.forEach((option, i) => {
+                            const template = this._options_menu.querySelector(".template");
+                            const new_button = template.cloneNode(true);
+                            new_button.classList.remove("template");
+                            new_button.textContent = option;
+                            new_button.display = "block";
+                            this._options_menu.appendChild(new_button);
+                            response_divs.push(new_button);
+
+                            // clicking on a button resolves with the index of the button clicked (so we know which option was chosen)
+                            new_button.addEventListener("click", () => {
+                                // aborts the event listener so it doesn't resolve multiple times if multiple buttons are clicked
+                                controller.abort();
+
+                                resolve(i);
+                                this._options_menu.style.bottom = "-100%";
+                                
+                                // delete all other dialogue options
+                                setTimeout(() => {
+                                    for (const div of response_divs) {
+                                        div.remove();
+                                    }
+                                }, 1000);
+                            }, { signal: controller.signal });
+                        });
+                        // animates the options menu sliding up from bottom
+                        this._options_menu.style.bottom = "0";
+                    }
                 }
                 // smoothly scrolls the text box to the bottom
                 text_box.scrollTo({
@@ -61,17 +106,5 @@ export default class Dialogue {
                 index++
             }, text_speed);
         });
-    }
-    
-    // reveals multiple texts in sequence with a delay between them
-    // also a promise
-    async reveal_multiple_texts(text_speed = 30, delay_between_texts = 1000, texts) {
-        return new Promise(async (resolve) => {
-            for (const text of texts) {
-                await this.reveal_text(text, text_speed);
-                await new Promise(resolve => setTimeout(resolve, delay_between_texts));
-            }
-            resolve('finished');
-        })
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,43 @@ import Dialogue from "./dialogue.js";
 const yahu_dialogue = new Dialogue("Benjamin Netanyahu", "../resources/images/netanyahu.png");
 
 await yahu_dialogue.show();
-await yahu_dialogue.reveal_multiple_texts(30, 1000, [
-    "Hi! Im your boss, Bejamin Netanyahu.",
-    "I have a very important task for you.",
-    "I need you to sell potions and make money for me.",
-    "Thanks brojob"
-]);
-yahu_dialogue.hide();
+
+const dialogue_options = [
+    [
+        'Ok cool',
+        "No you aren't dude"
+    ],
+    [
+        'Sure thing boss',
+        'I dont wanna do that'
+    ]
+]
+
+const text_speed = 30;
+const dialogue_chosen = await yahu_dialogue.reveal_text("Hi! Im your boss, Bejamin Netanyahu.", text_speed, dialogue_options[0]);
+
+// first dialogue options
+let next_text
+switch (dialogue_chosen) {
+    case 0:
+        next_text = "Great! Now make potions for me";
+        break;
+    case 1:
+        next_text = "Yeah too bad brojob, you're gonna make potions for me anyway";
+        break;
+}
+await yahu_dialogue.reveal_text(next_text, text_speed, dialogue_options[1]);
+
+// second dialogue options
+switch (dialogue_chosen) {
+    case 0:
+        next_text = "Thats the spirit!";
+        break;
+    case 1:
+        next_text = "Yo dude I really dont care what you wanna do, just make the potions for me";
+        break;
+}
+await yahu_dialogue.reveal_text(next_text);
+
+
+await yahu_dialogue.hide(1000);
